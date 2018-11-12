aafd5401872f105046c01334b7806c225ed29826
Rakesh Chotalia <Rakesh.Chotalia@imgtec.com>
2018-11-12 09:10:08 +0000

    Initial Commit

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+import { getFullName } from './utils'
+import '../styles/index.scss'
+
+const firstName = getFullName('Rakesh Chotalia')
+
+console.log(firstName)
+
+class Name {
+    name = 'Anonymous'
+}
